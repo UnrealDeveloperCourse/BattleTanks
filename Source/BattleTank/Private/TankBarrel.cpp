3d7ff7c676c6773d12a2a4040fb1d57cd9cabee7
Jonas <jawabiscuit@users.noreply.github.com>
2017-12-10 11:23:48 -0500

    Using Clamp to Limit Values

--- Source/BattleTank/Private/TankBarrel.cpp
+++ Source/BattleTank/Private/TankBarrel.cpp
@@ -5,7 +5,17 @@
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
+	// Need to clamp RelativeSpeed for values being passed in such as "5"
+	auto RelativeSpeedClamped = FMath::Clamp<float>(RelativeSpeed, -1., 1.);
+
+	auto ElevationChange = RelativeSpeedClamped * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto NewElevationUnclamped = RelativeRotation.Pitch + ElevationChange;
 	
+	// Make sure the values are clamped so the new elevation does not exceed max elevation
+	
+	// Static method FMath::Clamp()
+	auto Elevation = FMath::Clamp<float>(NewElevationUnclamped, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
 
