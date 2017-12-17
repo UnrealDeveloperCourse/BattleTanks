1af2ec1339765133ddb15c91d7ed78ba2e7b231f
Jonas <jawabiscuit@users.noreply.github.com>
2017-12-17 14:46:57 -0500

    Physics Materials and Friction

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -9,4 +9,8 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %.2f"), *Name, Throttle)
 
 	// TODO: clamp throttle value
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
