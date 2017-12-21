2f4b51460fbb930f8eb77d6b8570cf25efe211a1
Jonas <jawabiscuit@users.noreply.github.com>
2017-12-20 21:34:50 -0500

    TankMovementComponent DefaultSubobject Added

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -5,9 +5,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	auto Name = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %.2f"), *Name, Throttle)
-
 	// TODO: clamp throttle value
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
